3f2a8afa346c19b75eb6d0c0f2e4cbd5b0ee1c21
FrankFangH <hf2371@nyu.edu>
2025-03-17 20:14:54 -0400

    tests for candles

--- tests/test_pybirthday.py
+++ tests/test_pybirthday.py
@@ -1,6 +1,5 @@
 import pytest
-from src.pybirthday.pybirthday import cake
-
+from src.pybirthday.pybirthday import cake, candle
 
 class Tests:
 
@@ -20,9 +19,148 @@ class Tests:
     # Test functions
     #
 
-    def test_cake(self):
-        name = "Alex"
-        words = "Happy Birthday!"
-        actual = cake(name, words)
-        expected = ""
-        assert actual == expected, "Expected True to be equal to True!"
+    # Test for Candles
+    def test_specific_candle(self):
+        """
+        Test if calling 'candle(n)' returns the correct candle at index 'n'.
+        """
+        # Check first candle
+        first_candle = candle(0)
+        expected_candle = "\n".join([
+            "   (  ",
+            "  ) ) ",
+            " ( (  ",
+            "  ) ) ",
+            "   V  ",
+            "   |  ",
+            " .-+-.",
+            " |   |",
+            " '---'"
+        ])
+        assert first_candle == expected_candle, f"Expected first candle, but got:\n{first_candle}"
+
+    def test_out_of_range(self):
+        """
+        Test if calling 'candle(n)' with 'n' greater than the available styles returns the last candle.
+        """
+        last_candle = candle(100)
+        expected_last_candle = "\n".join([
+            "     ,?    ",
+            "    ( :    ",
+            "     ;)    ",
+            "     |     ",
+            "   ,-.-j   ",
+            "  ;|   |   ",
+            "  `|   |   ",
+            "   |   `.  ",
+            "   |,; |; ,-.",
+            "   |U  | /();",
+            " f`-^-^-',-' ",
+            "  `-----'   "
+        ])
+        assert last_candle == expected_last_candle, f"Expected last candle, but got:\n{last_candle}"
+        
+    def test_random_candle(self):
+        """
+        Test if calling 'candle()' without arguments returns a valid candle from the list.
+        """
+        result = candle().split("\n")
+        # Define possible candle options
+        candle_styles = [
+            [
+                "   (  ",
+                "  ) ) ",
+                " ( (  ",
+                "  ) ) ",
+                "   V  ",
+                "   |  ",
+                " .-+-.",
+                " |   |",
+                " '---'"
+            ],
+            [
+                "        ,        ",
+                "       ( \\       ",
+                "        ) \\      ",
+                "       ((\\ )     ",
+                "      |`-;-._    ",
+                "      |`-.   )   ",
+                "      |   \\ (   ",
+                "      |    \\ )   ",
+                "      |    |/    ",
+                "      |    `|    ",
+                "      |     |    ",
+                "      |     |    ",
+                "      |     |    ",
+                "      |     |    ",
+                "      |_____|    ",
+                "    _(_______)_  ",
+                "   |           | ",
+                "   |_____________|"
+            ],
+            [
+                "              :              ",
+                "      .       :       .      ",
+                "       ::.   :::   .::       ",
+                "          ::  :   ::         ",
+                "           :  )  :           ",
+                "      :::::  (_)  :::::      ",
+                "         .: .-;_ :.          ",
+                "       .:   |   \\  :.        ",
+                "            |    )           ",
+                "            |   |     __     ",
+                "          . |   |   _) .(    ",
+                "         _\\/|   | _) .'_|    ",
+                "          -\\|   |) .'_(      ",
+                "        ~>>8o<<-'8o-'  `     ",
+                "         / /,\\-(  )``.,      ",
+                "         | \\.'\\/  ) :(       ",
+                "         \\   `'   `) (`      ",
+                "          `._____.'`./       "
+            ],
+            [
+                "                   . . ...                   ",
+                "                .:::'.`:::::.                ",
+                "               ::::::  '::::::               ",
+                "              :::::: J6  ::::::              ",
+                "              :::::  HMw  :::::              ",
+                "              :::::  WNM  :::::              ",
+                "              :::::: MAM ::::::              ",
+                "               :::::.`;'.:::::               ",
+                "                 ::Mmmmmmm;:                 ",
+                "                   KKKRRMMA                  ",
+                "                   KPPPP9NM                  ",
+                "                   K7IIYINN                  ",
+                "                   Klll1lNJ                  ",
+                "                   Klll1lNR                  ",
+                "                   Kl::1lJl                  ",
+                "                   L:::1:J                   ",
+                "                   L:::!:J                   ",
+                "                   L:::::l                   ",
+                "                   l:..::l       .           ",
+                "             ,nnnnml:...:lmncJP',            ",
+                "  :::::::;mCCCc'pdPl:...:l9bqPyj'jm;:::::::::::::::",
+                "  ::::::OPCCcc.9b::;.....;::dP ,JCC9O::::::::::::::",
+                "  ::::' 98CCccc.9MkmmnnnmmJMP.JacOB8P '::::::::::::",
+                "  :::    `9qmCcccc\"\"YAAAY\"\"roseCmpP'    :::::::::::",
+                "  :::.     ``9mm,...     ...,mmP''     .:::::::::::",
+                "  :::::..       \"YTl995PPPT77\"      ..:::::::::::::",
+                "  :::::::::...                 ...:::::::::::::::::",
+                "  ::::::::::::::::.........::::::::::::::::::::::::"
+            ],
+            [
+                "     ,?    ",
+                "    ( :    ",
+                "     ;)    ",
+                "     |     ",
+                "   ,-.-j   ",
+                "  ;|   |   ",
+                "  `|   |   ",
+                "   |   `.  ",
+                "   |,; |; ,-.",
+                "   |U  | /();",
+                " f`-^-^-',-' ",
+                "  `-----'   "
+            ]
+        ]
+        assert any(result == candle for candle in candle_styles)
